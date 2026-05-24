4a68869937cf0191926ce8dccf17e5eeb6fb8f27
Cozy91 <suryanshguleria3@gmail.com>
2026-05-24 17:34:57 +0530

    added sdl rendering

--- include/Chip8.h
+++ include/Chip8.h
@@ -162,3 +162,224 @@ tableF[0x33] = &Chip8::OP_Fx33;
 tableF[0x55] = &Chip8::OP_Fx55;
 tableF[0x65] = &Chip8::OP_Fx65;
 
+};
+
+class Platform
+{
+  public:
+
+Platform(char const* title,int windowWidth,int windowHeight,int textureWidth,int textureHeight){
+  SDL_INIT(SDL_INIT_VIDEO); //initialising video flag
+
+  window=SDL_CreateWindow(title,windowWidth,windowHeight,SDL_WINDOW_FULLSCREEN); //window
+
+  renderer=SDL_CreateRenderer(window,nullptr);j //chooses best renderer for the system automatically
+  texture=SDL_createTexture(renderer, SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_STREMING,textureWidth,textureHeight);
+  
+}
+~Platform()
+{
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+}
+void update(void const* buffer,int pitch){
+  SDL_UpdateTexture(texture,nullptr,buffer,pitch);
+  SDL_RenderClear(renderer); //Clear the current rendering target with the drawing color.
+  SDL_RenderTexture(renderer,texture,nullptr,nullptr);
+  SDL_RenderPresent(renderer); // Update the screen with any rendering performed since the previous call 
+}
+bool ProcessInput(uint8_t* keys){
+  bool quit=false;
+  SDL_Event event; //struct 
+  while(SDL_PollEvent(&event)) //poll for currently pending events,queue
+  {
+    switch (event.type){
+    case SDL_Event_Quit:
+      quit=true;
+        break;
+  
+    case SDL_Event_KEY_DOWN:
+        switch(event.key.keysym.sym){
+          
+						case SDLK_ESCAPE:
+						{
+							quit = true;
+						} break;
+
+						case SDLK_x:
+						{
+							keys[0] = 1;
+						} break;
+
+						case SDLK_1:
+						{
+							keys[1] = 1;
+						} break;
+
+						case SDLK_2:
+						{
+							keys[2] = 1;
+						} break;
+
+						case SDLK_3:
+						{
+							keys[3] = 1;
+						} break;
+
+						case SDLK_q:
+						{
+							keys[4] = 1;
+						} break;
+
+						case SDLK_w:
+						{
+							keys[5] = 1;
+						} break;
+
+						case SDLK_e:
+						{
+							keys[6] = 1;
+						} break;
+
+						case SDLK_a:
+						{
+							keys[7] = 1;
+						} break;
+
+						case SDLK_s:
+						{
+							keys[8] = 1;
+						} break;
+
+						case SDLK_d:
+						{
+							keys[9] = 1;
+						} break;
+
+						case SDLK_z:
+						{
+							keys[0xA] = 1;
+						} break;
+
+						case SDLK_c:
+						{
+							keys[0xB] = 1;
+						} break;
+
+						case SDLK_4:
+						{
+							keys[0xC] = 1;
+						} break;
+
+						case SDLK_r:
+						{
+							keys[0xD] = 1;
+						} break;
+
+						case SDLK_f:
+						{
+							keys[0xE] = 1;
+						} break;
+
+						case SDLK_v:
+						{
+							keys[0xF] = 1;
+						} break; //copy pasted this one case
+					}
+        break;
+
+case SDL_KEYUP:
+				{
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_x:
+						{
+							keys[0] = 0;
+						} break;
+
+						case SDLK_1:
+						{
+							keys[1] = 0;
+						} break;
+
+						case SDLK_2:
+						{
+							keys[2] = 0;
+						} break;
+
+						case SDLK_3:
+						{
+							keys[3] = 0;
+						} break;
+
+						case SDLK_q:
+						{
+							keys[4] = 0;
+						} break;
+
+						case SDLK_w:
+						{
+							keys[5] = 0;
+						} break;
+
+						case SDLK_e:
+						{
+							keys[6] = 0;
+						} break;
+
+						case SDLK_a:
+						{
+							keys[7] = 0;
+						} break;
+
+						case SDLK_s:
+						{
+							keys[8] = 0;
+						} break;
+
+						case SDLK_d:
+						{
+							keys[9] = 0;
+						} break;
+
+						case SDLK_z:
+						{
+							keys[0xA] = 0;
+						} break;
+
+						case SDLK_c:
+						{
+							keys[0xB] = 0;
+						} break;
+
+						case SDLK_4:
+						{
+							keys[0xC] = 0;
+						} break;
+
+						case SDLK_r:
+						{
+							keys[0xD] = 0;
+						} break;
+
+						case SDLK_f:
+						{
+							keys[0xE] = 0;
+						} break;
+
+						case SDLK_v:
+						{
+							keys[0xF] = 0;
+						} break; //copy pasted this case too
+        }
+} return quit;
+}
+}
+}
+private:
+SDL_Window* window{};
+SDL_Renderer* renderer{};
+SDL_Texture* texture{};
+};
+
